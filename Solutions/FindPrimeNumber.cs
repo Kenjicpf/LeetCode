f9ec51d91f30428ae728021fb52651abe09b2135
kenjicpf <92.kenjicheung@gmail.com>
2019-03-18 16:10:47 +1300

    no message

---
+++ Solutions/FindPrimeNumber.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class FindPrimeNumber
+    {
+        public int FindLastPrimeNumber(int n)
+        {
+            var primes = new List<int>();
+            var number = 1;
+            while (primes.Count < n)
+            {
+                if (number.Equals(2))
+                {
+                    primes.Add(number);
+                    number++;
+                }
+                else if (number % 2 == 0)
+                {
+                    number++;
+                }
+                else
+                {
+                    if (primes.Count == 0)
+                    {
+                        number++;
+                    }
+                    var add = true;
+                    for (var j = 0; j < primes.Count; j++)
+                    {
+                        if (number % primes[j] == 0)
+                        {
+                            number++;
+                            add = false;
+                            break;
+                        }
+                    }
+                    if (add)
+                    {
+                        primes.Add(number);
+                        number++;
+                        add = false;
+                    }
+                }
+
+            }
+
+            return primes.Last();
+        }
+    }
+}
--- Solutions/Program.cs
+++ Solutions/Program.cs
@@ -10,8 +10,17 @@ namespace Solutions
     {
         static void Main(string[] args)
         {
-            var test = new ImplementStr();
-            Console.WriteLine(test.StrStr("", "a"));
+            //var test = new ImplementStr();
+            //Console.WriteLine(test.StrStr("", "a"));
+            //Console.ReadLine();
+
+
+
+            
+
+
+
+            Console.WriteLine(primes.Last());
             Console.ReadLine();
         }
     }
